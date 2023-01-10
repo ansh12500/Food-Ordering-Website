3608a9bdd72e5d3df0dacf7bedb21c8116de2ad7
Anand Shukla <114645060+ansh12500@users.noreply.github.com>
2023-01-10 07:45:00 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:yummy-food@gmail.com">yummy-food@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 10:00pm</p>
+      </div>
+
+      <div class="box"> 
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Mumbai, India - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+      </div>
+
+   </section>
+   <!-- <div class="credit">&copy; copyright @ (date('Y');) by <span>yummy-food</span> | all rights reserved!</div> -->
+   <div class="credit">&copy; Copyright @ 2022 By <span>yummy-food</span> | All Rights Reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
